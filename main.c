e59d9e52828aeaa164ded3b294abdc637f8f5cc9
Toshko Todorov <tttodorov@uni-sofia.bg>
2021-01-21 15:57:28 +0200

    Add condition

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/* *
+ * Напишете функция void itob(int n, char s[], int b), 
+ * която преобразува цяло число n в низ s базиран на основа b. 
+ * Например itob( n, s[], 16) представя числото n като шеснадесетично число в стринга s[].
+ * */
+
 void itob(int n, char s[], int b);
 
 int main(void) {
